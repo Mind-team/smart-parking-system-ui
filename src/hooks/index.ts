25a5c014f9d190d11d02d12cab0c2a9bd6c16218
Ermolaev Ilya <Ermolaev.ID@yandex.ru>
2022-05-14 11:03:43 +0500

    upgraded state mvp

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export * from "./debounce.hook";
 export * from "./formatter/formatter.hook";
+export * from "./upgradedState/upgradedState.hook";
---
+++ src/hooks/upgradedState/upgradedState.hook.ts
@@ -0,0 +1,55 @@
+import { SetStateAction } from "react";
+import { useState } from "react";
+
+export interface IUseUpgradedStateReturn<T> {
+  value: T;
+  setValue: (fn: SetStateAction<T | undefined>, changeLoading: boolean) => void;
+  isLoading: boolean;
+  setLoading: (value: boolean) => void;
+  toggleLoading: () => void;
+  isError: boolean;
+  serError: (value: boolean) => void;
+  toggleError: () => void;
+}
+
+export const useUpgradedState = <T>(
+  initialValue?: T,
+): IUseUpgradedStateReturn<T> => {
+  const [_value, _setValue] = initialValue
+    ? useState<T>(initialValue)
+    : useState<T>();
+  const [_isValueLoading, _setValueLoading] = useState(true);
+  const [_isValueError, _setValueError] = useState(false);
+
+  const setValue = (
+    fn: SetStateAction<T | undefined>,
+    changeLoading = true,
+  ) => {
+    // @ts-ignore
+    _setValue(fn);
+    if (changeLoading) {
+      _setValueLoading(false);
+    }
+  };
+
+  const toggle = (entity: "loading" | "error") => {
+    if (entity === "loading") {
+      _setValueLoading(!_isValueLoading);
+    }
+    if (entity === "error") {
+      _setValueError(!_isValueError);
+    }
+  };
+
+  return {
+    // @ts-ignore
+    value: _value,
+    setValue,
+    isLoading: _isValueLoading,
+    setLoading: _setValueLoading,
+    toggleLoading: toggle.bind(null, "loading"),
+    isError: _isValueError,
+    serError: _setValueError,
+    toggleError: toggle.bind(null, "error"),
+  };
+};
